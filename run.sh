659f59e502699d7bfa216b40ba198d2ca59d2905
Ellvius <lishinvs6541@gmail.com>
2025-11-05 01:07:59 +0530

    Add script

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail  # safer: stop on errors, undefined vars, and failed pipes
+
+# Config
+PROJECT_ROOT="$(pwd)"
+
+# To generate the output xsm code
+pushd abstree >/dev/null
+yacc -d abstree.y
+lex abstree.l
+popd >/dev/null
+
+gcc -g abstree/lex.yy.c \
+    abstree/y.tab.c \
+    abstree/abstree.c \
+    codegen/codegen.c \
+    symboltable/symboltable.c \
+    -o "$PROJECT_ROOT/expl"
+
+"$PROJECT_ROOT/expl" "${1:-}"
